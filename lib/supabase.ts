fe659fe3d4f8cab159f835e89bb62afa5ba94289
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-10-02 09:47:52 -0300

    Passo para o Deploy correto no coolify e migrado Keys para o .env (foram resetadas)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "http://supabasekong-y84swckocg0oswok80k8wg84.144.33.31.46.sslip.io"
-const supabaseKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTcyNzQzODU4MCwiZXhwIjo0ODgzMTEyMTgwLCJyb2xlIjoic2VydmljZV9yb2xlIn0.bGlrUoJRvCDim80bB6NaZUmzPmMmRmt8j8I0EEyZIgo"
+// Usar valor padrão ou lançar um erro se não estiver definido
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || ''
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
