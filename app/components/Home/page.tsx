6615a9beca121396278624317ccb126657c1d01f
Informática Flecksteel <informatica@flecksteel.com.br>
2025-01-09 16:44:26 +0000

    Alterado a Home para funcionar dinamente com o Pocketbase. Próxmo passo - Tela de cadastro e manejo dos banners da Home

--- app/components/Home/page.tsx
+++ app/components/Home/page.tsx
@@ -1,56 +1,50 @@
-'use client'
+'use client';
 
-import { useState } from 'react'
-import Image from "next/image"
-import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent } from "@/components/ui/dialog"
+import { useState, useEffect } from 'react';
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { X } from "lucide-react"
+import { X } from "lucide-react";
 
-// Imagens Temporárias
-import img1 from '@/assets/imagem1.jpg'
-import img2 from '@/assets/imagem2.jpg'
-import img3 from '@/assets/imagem3.jpg'
-import natal from '@/assets/natal.jpeg'
+import Cardapio from '../Cardapio/page';
 
-import Cardapio from '../Cardapio/page'
+import PocketBase from "pocketbase";
 
-const carouselData = [
-  {
-    id: 1,
-    title: "Bem-vindo à nova Intranet do Grupo Plínio Fleck!",
-    description: "Criamos este espaço para tornar o seu dia a dia mais prático e eficiente. Aqui, você encontrará facilmente os recursos mais importantes para o seu trabalho, como documentos, informações de auditoria e ferramentas essenciais para a rotina. Além disso, a nova Intranet facilita o acesso às novidades e comunicados da empresa, tudo de forma integrada e organizada. Nosso objetivo é oferecer um ambiente digital que apoie suas atividades e promova a colaboração entre as equipes. Explore e aproveite todos os benefícios desta nova plataforma!",
-    imageUrl: img1
-  },
-  {
-    id: 2,
-    title: "Inauguração da Nova Área de Lazer Nayr Anna Fleck",
-    description: "Celebramos a inauguração da nova Área de Lazer Nayr Anna Fleck, um espaço projetado para promover momentos de descontração e convivência entre os colaboradores. O local recebe o nome em homenagem à Nayr Anna Fleck, esposa do fundador do Grupo Plínio Fleck, reconhecida por seu legado de apoio e dedicação. Aproveite para conhecer e desfrutar deste ambiente pensado com carinho para todos nós!",
-    imageUrl: img2
-  },
-  {
-    id: 3,
-    title: "Inauguração do Novo Auditório Plínio Fleck",
-    description: "Com grande entusiasmo, anunciamos a inauguração do novo Auditório Plínio Fleck, uma homenagem ao fundador do Grupo, Sr. Plínio Fleck. Este espaço foi cuidadosamente projetado para oferecer conforto e tecnologia audiovisual, sendo ideal para reuniões, eventos e treinamentos que fortalecem a nossa trajetória de sucesso. Venha conhecer e fazer parte deste marco em nossa história!",
-    imageUrl: img3
-  },
-  {
-    id: 4,
-    title: "Feliz Natal e Próspero Ano Novo!",
-    description: "Agradecemos a todos os colaboradores que estiveram conosco ao longo deste ano, contribuindo para o nosso crescimento e sucesso. Desejamos que este Natal seja repleto de alegria, amor e união, e que o ano novo traga novas oportunidades e realizações para todos. Que 2024 seja ainda mais próspero e cheio de conquistas. Boas festas e um feliz ano novo!",
-    imageUrl: natal
-  }
-];
-
-
+const pb = new PocketBase("https://pocketbase.flecksteel.com.br");
 
 export default function Home() {
-  const [currentSlide, setCurrentSlide] = useState(0)
-  const [isModalOpen, setIsModalOpen] = useState(false)  
+  const [carouselData, setCarouselData] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Fetch dados do PocketBase
+  useEffect(() => {
+    const fetchCarouselData = async () => {
+      try {
+        const data = await pb.collection("carrocel").getList(1, 50, {
+          filter: 'ativo = true', // Filtra somente os registros onde ativo é true
+          sort: "-updated", // Ordena pelos mais recentes
+        });        
+        // Adiciona a URL base para os banners
+        const formattedData = data.items.map((item) => ({
+          id: item.id,
+          title: item.titulo,
+          description: item.descricao,
+          imageUrl: `${pb.baseUrl}/api/files/carrocel/${item.id}/${item.banner}`,
+        }));
+        setCarouselData(formattedData);
+      } catch (error) {
+        console.error("Erro ao buscar dados do PocketBase:", error);
+      }
+    };
+
+    fetchCarouselData();
+  }, []);
 
   return (
     <div>
@@ -58,31 +52,28 @@ export default function Home() {
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           navigation
-          pagination={{ 
-            clickable: true,
-           }}
+          pagination={{ clickable: true }}
           loop
           className="w-full h-full rounded-lg flex"
-
         >
           {carouselData.map((slide, index) => (
-            <SwiperSlide key={index} className="!flex !flex-row items-center h-auto relative">
+            <SwiperSlide key={slide.id} className="!flex !flex-row items-center h-auto relative">
               {/* Retângulos com as cores da bandeira do RS */}
               <div className="absolute left-0 top-0 h-full flex">
                 <div className="bg-green-300 w-2"></div> {/* Verde */}
                 <div className="bg-yellow-300 w-2"></div> {/* Amarelo */}
                 <div className="bg-red-500 w-2"></div> {/* Vermelho */}
-                <div className="bg-gray-400 w-2"></div> {/* Vermelho */}
+                <div className="bg-gray-400 w-2"></div> {/* Cinza */}
               </div>
-              <div className="w-2/4 p-6 ml-4 min-h-full! flex flex-col justify-between"> {/* Adiciona `min-h-full` para garantir altura mínima */}
+              <div className="w-2/4 p-6 ml-4 flex flex-col justify-center">
                 <h2 className="text-3xl font-bold mb-4">{slide.title}</h2>
-                <p className="text-lg text-justify flex-grow">{slide.description}</p> {/* `flex-grow` faz o texto expandir */}
+                <p className="text-lg text-justify">{slide.description}</p>
               </div>
               <div className="w-2/4 relative h-full aspect-[16/9]">
                 <Image
                   src={slide.imageUrl}
                   alt={`Slide image ${index + 1}`}
-                  fill={true}
+                  fill={true} // Usa o modo de preenchimento
                   className="object-cover rounded-xl"
                   onClick={() => {
                     setIsModalOpen(true);
@@ -95,21 +86,26 @@ export default function Home() {
         </Swiper>
       </section>
 
-
-
       <Cardapio />
 
       <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-        <DialogContent className="p-0 absolute rounded-lg">
-          <div className="w-full h-full">
-            <Image
-              src={carouselData[currentSlide].imageUrl}
-              alt={carouselData[currentSlide].title}
-              sizes="100vw 100vh"
-              className='rounded-lg'
-              style={{
-                objectFit: "contain"
-              }} />
+        <DialogContent
+          className="p-0 rounded-lg w-full max-w-4xl h-auto"
+          style={{
+            maxHeight: '90vh', // Limita a altura máxima do modal
+          }}
+        >
+          <div className="relative w-full h-full">
+            {carouselData[currentSlide] && (
+              <div className="relative h-[80vh] w-full">
+                <Image
+                  src={carouselData[currentSlide].imageUrl}
+                  alt={carouselData[currentSlide].title}
+                  fill={true} // Usa o modo de preenchimento
+                  className="object-contain rounded-lg"
+                />
+              </div>
+            )}
             <Button
               variant="outline"
               size="icon"
